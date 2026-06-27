cae3a733fd883c32168375a19053da0ca206d9a2
root <root@baapqtol.colocrossing.cloud>
2026-06-27 15:27:03 +0800

    fix: add Lucky backend and mount-utils for immortawrt build
    - Clone lucky backend (gdy666/luci-app-lucky) to package/lucky
    - Enable CONFIG_PACKAGE_lucky=y
    - Enable CONFIG_PACKAGE_mount-utils=y (iStore dependency)

--- scripts/immortalwrt/prepare.sh
+++ scripts/immortalwrt/prepare.sh
@@ -68,6 +68,7 @@ p "添加 argon 主题 feed"
 git clone --depth 1 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
 git clone --depth 1 https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
 git clone --depth 1 https://github.com/gdy666/luci-app-lucky.git package/lucky
+git clone --depth 1 https://github.com/gdy666/luci-app-lucky.git package/luci-app-lucky
 
 p "更新 Feeds"
 ./scripts/feeds update -f -a
